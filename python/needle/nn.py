dc1b78fa88bac1dc22e8b2716aa213e5372db075
Justin Chu <justinchuby@users.noreply.github.com>
2022-11-01 02:00:45 +0000

    Snapshot

--- python/needle/nn.py
+++ python/needle/nn.py
@@ -105,9 +105,9 @@ class Linear(Module):
             in_features, out_features, device=device, dtype=dtype, requires_grad=True
         )
         if bias:
-            self.bias = init.kaiming_uniform(
+            self.bias = ops.transpose(init.kaiming_uniform(
                 out_features, 1, device=device, dtype=dtype, requires_grad=True
-            )
+            ))
         else:
             self.bias = None
         ### END YOUR SOLUTION
