2ea95719c48a3581fe6a25a22a98a70624c00c15
Justin Chu <justinchuby@users.noreply.github.com>
2022-11-03 23:48:07 +0000

    Dropout

--- python/needle/nn.py
+++ python/needle/nn.py
@@ -220,9 +220,11 @@ class Dropout(Module):
         self.p = p
 
     def forward(self, x: Tensor) -> Tensor:
-        ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
-        ### END YOUR SOLUTION
+        if not self.training:
+            return x
+        # Create a zeroed mask
+        mask = init.randb(*x.shape, p=self.p, device=x.device, dtype=x.dtype)
+        return x * mask
 
 
 class Residual(Module):
